fcff389c589f78a5ced44cb4304cda980a3694e7
romain sanson <romainsanson.gm@gmail.com>
2019-11-08 13:57:52 +0100

    ajaxify dashboard search

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -2,7 +2,17 @@ class DashboardsController < ApplicationController
 
   def user_dashboard
     @articles = current_user.articles
-    @pagy, @articles = pagy(helpers.dashboard_search(@articles, params), items: 10)
+    @pagy, @articles = pagy(
+      helpers.dashboard_search(@articles, params),
+      items: 10,
+      link_extra: 'data-remote="true"'
+    )
+    if params[:filter_data]
+      respond_to do |format|
+        format.html { redirect_to profile_path }
+        format.js
+      end
+    end
   end
 
   private
