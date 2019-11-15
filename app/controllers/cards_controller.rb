51246a0778ad1feb26f606a5ea77ede9f8c48de4
romain sanson <romainsanson.gm@gmail.com>
2019-11-15 18:11:52 +0100

    working on species and animals

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -21,27 +21,30 @@ class CardsController < ApplicationController
   def show
     authorize @card
 
-    @card_animal = @card.animal
-    @recomandations = Card.where(animal: @card.animal).sample(3)
+    @animal = @card.specie.animal.name
+    # @recomandations = Card.where(specie: @card.specie).sample(3)
   end
 
   def new
-    @card = Card.new(card_params)
+    @card = Card.new
     authorize @card
   end
 
   def create
     @card = Card.new(card_params)
     @card.set_specs_and_ratings(params)
+    @card.specie = set_specie(params)
     authorize @card
-    if @card.save
+    raise
+    if @card.save!
       redirect_to card_path(@card)
     else
-      render :new
+      render :new, animal: params[:animal]
     end
   end
 
   def edit
+    params[:animal] = Card.find(params[:id]).specie.animal.name
     authorize @card
   end
 
@@ -63,11 +66,20 @@ class CardsController < ApplicationController
 
   private
 
+  def set_specie(params)
+    if !params[:card][:specie].empty?
+      Specie.create(
+        animal: Animal.find_by(name: params[:animal]),
+        name: params[:card][:specie]
+      )
+    end
+  end
+
   def set_card
     @card = Card.find(params[:id])
   end
 
   def card_params
-    params.require(:card).permit(:animal, :origin, :min_height, :max_height, :min_weight, :max_weight, :title, :body, :life_expectancy, :thumbnail)
+    params.require(:card).permit(:origin, :min_height, :max_height, :min_weight, :max_weight, :title, :body, :life_expectancy, :thumbnail)
   end
 end
--- app/models/card.rb
+++ app/models/card.rb
@@ -6,15 +6,15 @@ class Card < ApplicationRecord
   STATUS = %w(draft published submitted)
 
   validates :status, inclusion: { in: STATUS }
-  validates :thumbnail, presence: true
-  validates :title, presence: true
+  # validates :thumbnail, presence: true
+  # validates :title, presence: true
   validates :body, presence: true
-  validates :origin, presence: true
-  validates :life_expectancy, presence: true
-  validates :min_height, presence: true
-  validates :max_height, presence: true
-  validates :min_weight, presence: true
-  validates :max_weight, presence: true
+  # validates :origin, presence: true
+  # validates :life_expectancy, presence: true
+  # validates :min_height, presence: true
+  # validates :max_height, presence: true
+  # validates :min_weight, presence: true
+  # validates :max_weight, presence: true
 
 
   def set_specs_and_ratings(params)
