c192e7b825f8dc2ca971a8921d2a4b3db024a8f5
romain sanson <romainsanson.gm@gmail.com>
2019-11-15 22:40:40 +0100

    some refacto

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -12,8 +12,8 @@ class ArticlesController < ApplicationController
 
     @pagy, @articles = pagy(
       helpers.index_search(@articles, params),
-      items: 10
-      # link_extra: 'data-remote="true"'
+      items: 10,
+      link_extra: 'data-remote="true"'
     )
   end
 
