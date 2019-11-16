b260ca41637916a913a0267cfa9ee4d423ab1865
romain sanson <romainsanson.gm@gmail.com>
2019-11-16 13:55:08 +0100

    ajax on admin publish!

--- app/controllers/admin/articles_controller.rb
+++ app/controllers/admin/articles_controller.rb
@@ -2,7 +2,18 @@ class Admin::ArticlesController < ApplicationController
   def publish
     @article = Article.find(params[:article_id])
     @article.update(status: 'published')
-    redirect_to admin_profile_path
+
+    @articles = Article.all
+    @pagy, @articles = pagy(
+      helpers.admin_search(@articles, params),
+      items: 10,
+      link_extra: 'data-remote="true"'
+    )
+
+    respond_to do |format|
+      format.html { redirect_to admin_profile_path }
+      format.js
+    end
   end
 
   def unpublish
