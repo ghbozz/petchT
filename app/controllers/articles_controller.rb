4836f1118a8812522405be661ec83553b801303c
romain sanson <romainsanson.gm@gmail.com>
2019-11-28 21:59:31 +0100

    tweak article update for admin

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -33,12 +33,13 @@ class ArticlesController < ApplicationController
   end
 
   def create
+    authorize @article
+
     params[:article][:tag_list] = set_tags(params[:tags])
     @article = Article.new(article_params.merge(user: current_user))
     @article.animal = Animal.find_by(name: params[:article][:animal])
     @article.update(status: 'submitted') if params[:commit] == 'Poster'
 
-    authorize @article
     if @article.save
       params
       redirect_to article_path(@article)
@@ -52,11 +53,13 @@ class ArticlesController < ApplicationController
   end
 
   def update
+    authorize @article
+
     params[:article][:tag_list] = set_tags(params[:tags])
-    @article.update(article_params.merge(status: 'draft'))
+    @article.update(article_params)
+    @article.update(status: 'draft') if !current_user.is_admin?
     @article.update(status: 'submitted') if params[:commit] == 'Poster'
 
-    authorize @article
 
     if @article.save
       redirect_to article_path(@article)
