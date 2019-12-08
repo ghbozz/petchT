cd2530a224e965f9407301bc838a340087b0a6a9
romain sanson <romainsanson.gm@gmail.com>
2019-12-08 21:12:11 +0100

    m

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,7 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!
 
   def home
-    @articles = policy_scope(Article).where(status: 'published').last(25)
+    @articles = Article.where(status: 'published').last(25)
 
     @top_articles = Article.where(top: true).limit(3)
   end
