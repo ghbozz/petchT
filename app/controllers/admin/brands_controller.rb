1ac96f639047e11a508a5317e5c864d67f559823
romain sanson <romainsanson.gm@gmail.com>
2019-12-13 23:35:18 +0100

    fix brand update

--- app/controllers/admin/brands_controller.rb
+++ app/controllers/admin/brands_controller.rb
@@ -19,7 +19,7 @@ class Admin::BrandsController < ApplicationController
   end
 
   def update
-    @brand = Brand.find(params[:id])
+    @brand = Brand.friendly.find(params[:id])
     @brand.update(brand_params)
     @brand.set_targets(params)
     redirect_to brand_path(@brand)
