08cef6247190c026840d6b51a6c89b5f579db1bb
romain sanson <romainsanson.gm@gmail.com>
2019-12-16 10:39:18 +0100

    order articles on home

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,7 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!
 
   def home
-    @articles = Article.published.last(25)
+    @articles = Article.published.order(created_at: :desc).last(21)
     @top_articles = Article.top
   end
 end
